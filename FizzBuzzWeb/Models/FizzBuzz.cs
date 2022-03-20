e2a665c1029d596de6bc90c722819a8f3c5edd05
szymo <szymo@LAPTOP-3NGDP6FU>
2022-03-20 23:47:16 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+
+        public int? Number { get; set; }
+
+        [Display]
+        public string Result { get; set; }
+    }
+}
---
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using FizzBuzzWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public string Result;
+
+        public string Alert;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DefaultName();
+        }
+
+        public void DefaultName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                if (FizzBuzz.Number > 1000 || FizzBuzz.Number < 1 || FizzBuzz.Number == null)
+                {
+                    Result = null;
+                    Alert = "alert alert-danger";
+                    DefaultName();
+                }
+                else if (FizzBuzz.Number % 15 == 0)
+                {
+                    Result = "FizzBuzz";
+                    Alert = "alert alert-success";
+                    DefaultName();
+                }
+                else if (FizzBuzz.Number % 3 == 0)
+                {
+                    Result = "Fizz";
+                    Alert = "alert alert-success";
+                    DefaultName();
+                }
+                else if (FizzBuzz.Number % 5 == 0)
+                {
+                    Result = "Buzz";
+                    Alert = "alert alert-success";
+                    DefaultName();
+                }
+                else
+                {
+                    Result = "Wartość " + FizzBuzz.Number + " nie spełnia kryteriów FizzBuzz.";
+                    Alert = "alert alert-danger";
+                    DefaultName();
+                }
+
+                FizzBuzz.Result = Result;
+            }
+
+            return Page();
+        }
+    }
+}
